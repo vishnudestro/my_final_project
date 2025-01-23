5815ed13118908851a3358cd3e867a80dbf4e641
vishnudestro <prakashvishnu45@gmail.com>
2025-01-23 18:17:51 +0530

    update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -10,7 +10,7 @@ sh './build.sh'
 #Docker login
 docker login -u vishnu2naick -p dckr_pat_nm4fLXJHR-4bYLgnn7fgXYqFa-E
 
-echo "Branch:{$GIT_BRANCH}"
+echo "Branch:$GIT_BRANCH"
 
 #Push image to Docker Hub
 if [[ $GIT_BRANCH == "origin/dev" ]]; then
@@ -18,7 +18,7 @@ if [[ $GIT_BRANCH == "origin/dev" ]]; then
     docker tag mob_shop_project:v1 vishnu2naick/dev
     docker push vishnu2naick/dev
 
-elif [[ $GIT_BRANCH == "origin/main" ]]|[[$GIT_BRANCH == "origin/master"]]; then
+elif [[ "$GIT_BRANCH" == "origin/main" ]]|[["$GIT_BRANCH" == "origin/master"]]; then
 
     docker tag mob_shop_project:v1 vishnu2naick/prod
     docker push vishnu2naick/prod
