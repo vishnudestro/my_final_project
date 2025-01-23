8a153b0754cdbb12406490064d9a4f8828c9baec
vishnudestro <prakashvishnu45@gmail.com>
2025-01-23 18:46:59 +0530

    update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,23 +4,23 @@
 sh './build.sh'
 
 #Build Docker Container
-# docker-compose up -d
-# docker ps
+docker-compose up -d
+docker ps
 
-#Docker login
-docker login -u vishnu2naick -p dckr_pat_nm4fLXJHR-4bYLgnn7fgXYqFa-E
+# #Docker login
+# docker login -u vishnu2naick -p dckr_pat_nm4fLXJHR-4bYLgnn7fgXYqFa-E
 
-echo "Branch:$GIT_BRANCH"
+# echo "Branch:$GIT_BRANCH"
 
-#Push image to Docker Hub
-if [ "$GIT_BRANCH" == "origin/dev" ]; then
+# #Push image to Docker Hub
+# if [ "$GIT_BRANCH" == "origin/dev" ]; then
 
-    docker tag mob_shop_project:v1 vishnu2naick/dev
-    docker push vishnu2naick/dev
+#     docker tag mob_shop_project:v1 vishnu2naick/dev
+#     docker push vishnu2naick/dev
 
-elif [ "$GIT_BRANCH" == "origin/main" ]|| ["$GIT_BRANCH" == "origin/master"]; then
+# elif [ "$GIT_BRANCH" == "origin/main" ] || ["$GIT_BRANCH" == "origin/master"]; then
 
-    docker tag mob_shop_project:v1 vishnu2naick/prod
-    docker push vishnu2naick/prod
+#     docker tag mob_shop_project:v1 vishnu2naick/prod
+#     docker push vishnu2naick/prod
 
-fi
+# fi
